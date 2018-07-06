e7a7848ae721651047edd54c2e88a9a31ed485eb
Chris Holdgraf <choldgraf@berkeley.edu>
2018-07-06 15:31:54 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from sphinx_copybutton import __version__
+
+setup(
+    name='sphinx-copybutton',
+    version=__version__,
+    author='Chris Holdgraf',
+    author_email='choldgraf@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+    package_data={'sphinx_copybutton': ['_static/copybutton.css', '_static/copybutton.js']},
+    include_package_data=True,
+)
---
+++ sphinx_copybutton.py
@@ -0,0 +1,10 @@
+import os
+
+__version__ = "0.1"
+github_url = 'https://raw.githubusercontent.com/choldgraf/sphinx-copybutton/master/_static/'
+
+def setup(app):
+    # Add relevant code to headers
+    app.add_stylesheet(github_url + 'copybutton.css')
+    app.add_javascript(github_url + "copybutton.js")
+    app.add_javascript("https://cdn.jsdelivr.net/npm/clipboard@1/dist/clipboard.min.js")
