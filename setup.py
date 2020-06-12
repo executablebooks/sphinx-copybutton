58e277ec7d9e4c5b5393c1d61e4487f2834cde25
Chris Sewell <chrisj_sewell@hotmail.com>
2020-06-12 22:20:53 +0100

    inject format func with jinja

--- setup.py
+++ setup.py
@@ -3,19 +3,22 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
-if (os.path.isdir('clipboard.js') and
-        not os.path.islink('sphinx_copybutton/_static/clipboard.min.js')):
+if os.path.isdir("clipboard.js") and not os.path.islink(
+    "sphinx_copybutton/_static/clipboard.min.js"
+):
     raise SystemExit("Error: Support for symbolic links is required")
 
-if (os.path.isdir('clipboard.js') and
-        not os.path.isfile('clipboard.js/dist/clipboard.min.js')):
+if os.path.isdir("clipboard.js") and not os.path.isfile(
+    "clipboard.js/dist/clipboard.min.js"
+):
     raise SystemExit(
         """Error: clipboard.js submodule not available, run
 
         git submodule update --init
-        """)
+        """
+    )
 
-with open('./README.md', 'r') as ff:
+with open("./README.md", "r") as ff:
     readme_text = ff.read()
 
 # Parse version
@@ -26,21 +29,27 @@ for line in init.read_text().split("\n"):
 version = line.split(" = ")[-1].strip('"')
 
 setup(
-    name='sphinx-copybutton',
+    name="sphinx-copybutton",
     version=version,
     description="Add a copy button to each of your code cells.",
     long_description=readme_text,
-    long_description_content_type='text/markdown',
-    author='Executable Book Project',
+    long_description_content_type="text/markdown",
+    author="Executable Book Project",
     url="https://github.com/ExecutableBookProject/sphinx-copybutton",
-    license='MIT License',
+    license="MIT License",
     packages=find_packages(),
-    package_data={'sphinx_copybutton': ['_static/copybutton.css',
-                                        '_static/copybutton.js_t',
-                                        '_static/copy-button.svg',
-                                        '_static/clipboard.min.js']},
+    package_data={
+        "sphinx_copybutton": [
+            "_static/copybutton.css",
+            "_static/copybutton_funcs.js",
+            "_static/copybutton.js_t",
+            "_static/copy-button.svg",
+            "_static/clipboard.min.js",
+        ]
+    },
     classifiers=["License :: OSI Approved :: MIT License"],
-    install_requires=[
-        "sphinx>=1.8"
-    ]
+    install_requires=["sphinx>=1.8"],
+    extras_require={
+        "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
+    },
 )
--- sphinx_copybutton/__init__.py
+++ sphinx_copybutton/__init__.py
@@ -1,27 +1,44 @@
 """A small sphinx extension to add "copy" buttons to code blocks."""
-import os
+from pathlib import Path
 from sphinx.util import logging
 
 __version__ = "0.2.12dev0"
 
 logger = logging.getLogger(__name__)
 
+
 def scb_static_path(app):
-    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))
-    app.config.html_static_path.append(static_path)
+    app.config.html_static_path.append(
+        str(Path(__file__).parent.joinpath("_static").absolute())
+    )
+
 
 def add_to_context(app, config):
     # Update the global context
-    config.html_context.update({'copybutton_prompt_text': config.copybutton_prompt_text})
-    config.html_context.update({'copybutton_only_copy_prompt_lines': config.copybutton_only_copy_prompt_lines})
-    config.html_context.update({'copybutton_remove_prompts': config.copybutton_remove_prompts})
-    config.html_context.update({'copybutton_image_path': config.copybutton_image_path})
-    config.html_context.update({'copybutton_selector': config.copybutton_selector})
+    config.html_context.update(
+        {"copybutton_prompt_text": config.copybutton_prompt_text}
+    )
+    config.html_context.update(
+        {"copybutton_only_copy_prompt_lines": config.copybutton_only_copy_prompt_lines}
+    )
+    config.html_context.update(
+        {"copybutton_remove_prompts": config.copybutton_remove_prompts}
+    )
+    config.html_context.update({"copybutton_image_path": config.copybutton_image_path})
+    config.html_context.update({"copybutton_selector": config.copybutton_selector})
+    config.html_context.update(
+        {
+            "copybutton_format_func": Path(__file__)
+            .parent.joinpath("_static", "copybutton_funcs.js")
+            .read_text()
+        }
+    )
+
 
 def setup(app):
-    logger.verbose('Adding copy buttons to code blocks...')
+    logger.verbose("Adding copy buttons to code blocks...")
     # Add our static path
-    app.connect('builder-inited', scb_static_path)
+    app.connect("builder-inited", scb_static_path)
 
     # configuration for this tool
     app.add_config_value("copybutton_prompt_text", "", "html")
@@ -34,9 +51,11 @@ def setup(app):
     app.connect("config-inited", add_to_context)
 
     # Add relevant code to headers
-    app.add_css_file('copybutton.css')
-    app.add_js_file('clipboard.min.js')
+    app.add_css_file("copybutton.css")
+    app.add_js_file("clipboard.min.js")
     app.add_js_file("copybutton.js")
-    return {"version": __version__,
-            "parallel_read_safe": True,
-            "parallel_write_safe": True}
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
