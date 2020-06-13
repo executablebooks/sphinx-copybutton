ffaffa563842cb4ed938b4bfe95daee692de3c25
Chris Sewell <chrisj_sewell@hotmail.com>
2020-06-13 01:11:30 +0100

    parameterise js tests

--- sphinx_copybutton/_static/test.js
+++ sphinx_copybutton/_static/test.js
@@ -1,13 +1,24 @@
 import test from 'ava';
 import { formatCopyText } from "./copybutton_funcs";
 
+const parameters = [
+	{
+		description: 'no prompt',
+		text: 'hallo',
+		prompt: '',
+		expected: 'hallo'
+	},
+	{
+		description: 'with prompt',
+		text: '>>> hallo',
+		prompt: '>>> ',
+		expected: 'hallo'
+	}
+]
 
-test('no prompt', t => {
-	const text = formatCopyText(['hallo'], '');
-	t.is(text, 'hallo')
-});
-
-test('with prompt', t => {
-	const text = formatCopyText(['>>> hallo'], '>>> ');
-	t.is(text, 'hallo')
-});
+parameters.forEach((parameter) => {
+	test(parameter.description, t => {
+		const text = formatCopyText(parameter.text.split('\n'), parameter.prompt);
+		t.is(text, parameter.expected)
+	});
+})
